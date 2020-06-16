0ca5e43edaafaf2f1ac8be2c112f1937aad723f8
okano0120 <okano0120@gmail.com>
2020-06-16 07:07:33 +0000

    Homeコントローラー作成

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module RailsTemplate
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
+
     config.generators.system_tests = nil
   end
 end
